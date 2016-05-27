fe6fb3631d864557bfdf112a14064393de96bdec
AndreeNY <a.alvarez.guzman@isur.edu.pe>
2016-05-27 16:16:57 -0500

    Interfaz
    No cargan las imagenes.
    Agregue Images y style. Luego copie el contenido del archivo index a un
    master page en creado en app web, en el proyecto.

--- SistemaInmobiliariaAndree/AppWeb/Site1.Master.vb
+++ SistemaInmobiliariaAndree/AppWeb/Site1.Master.vb
@@ -1,5 +1,5 @@
-﻿Public Class WebForm1
-    Inherits System.Web.UI.Page
+﻿Public Class Site1
+    Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
